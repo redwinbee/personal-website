b1407a14107a02924b64b012db153001742a80f2
Edwin Bermudes <hey@redwinbee.me>
2023-11-16 21:53:57 -0500

    another test commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 function App() {
+    let [value, setValue] = useState(0)
+
+    function handleClick() {
+        setValue(value + 1)
+    }
+
     return (
         <div className="app">
             <h1>Edwin Bermudes</h1>
             <p>This is a placeholder while the site is being constructed</p>
             <p>Come back another time =)</p>
+            <p>value = {value}</p>
+            <button onClick={handleClick}>Click Me!</button>
         </div>
     )
 }
