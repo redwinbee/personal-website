8335d1d5e037498b6aab6a18c5a2cb9d9d7c0a41
Edwin Bermudes <hey@redwinbee.me>
2023-11-18 13:13:20 -0500

    use prettier for the project

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import "./App.css";
 import Card from "./components/Card";
 
 export default function App() {
-    return (
-        <Container>
-            <Card title="Edwin Bermudes" body="This website is being written using React and TailwindCSS and is still very much a work in progress.
+  return (
+    <Container>
+      <Card
+        title="Edwin Bermudes"
+        body="This website is being written using React and TailwindCSS and is still very much a work in progress.
                     Thanks for stopping by and make sure to come back later! P.S: If you hover over the scrambled text
-                    above this paragraph, you can see what it says =P">
-                <a className="text-cyan-200" href="https://github.com/redwinbee">@GitHub</a>
-            </Card>
-        </Container>
-    )
+                    above this paragraph, you can see what it says =P"
+      >
+        <a className="text-cyan-200" href="https://github.com/redwinbee">
+          @GitHub
+        </a>
+      </Card>
+    </Container>
+  );
 }
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
 export default function Container({ children }) {
-    return (
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="shadow bg-neutral-700 w-1/2 py-3.5">
-                <div className="flex flex-auto mx-auto px-4">
-                    {children}
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="shadow bg-neutral-900 w-1/2 py-3.5">
+        <div className="flex flex-auto mx-auto px-4">{children}</div>
+      </div>
+    </div>
+  );
 }
